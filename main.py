a16fb3bf35356512f20fe646261fd041bba358f0
ali207715 <ali207715@gmail.com>
2024-01-12 16:18:30 +0000

    nill

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import random
 from datetime import datetime, timedelta
-# Chores and Candidates
+
 chores = ["GARBAGE + VACUUM", "MOPING THE FLOORS", "CLEANING THE TOILET + WIPING FURNITURE"]
 candidates = ["ALI", "SEN", "ANIKET"]
 
 
-# Function to randomly assign chores to candidates
 @st.cache_data(persist="disk")
 def assign_chores():
     random.shuffle(candidates)
@@ -25,16 +24,13 @@ if is_week_passed(last_assigned_timestamp):
     assign_chores.clear()
     last_assigned_timestamp, assigned_chores = assign_chores()
 
-# UI
 st.title("Weekly Chores Assignment")
 
-# Display assigned chores
 if assigned_chores:
     st.subheader("This week's assigned chores:")
     for chore, candidate in assigned_chores.items():
         st.info(f"{chore}: {candidate}")
 
-# Checklist for marking off completed chores
 st.subheader("Mark off completed chores:")
 completed_chores = st.checkbox("GARBAGE + VACUUM")
 if completed_chores:
